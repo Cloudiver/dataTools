d895915ea4c6bca3d1a1b5ac4328ecde400f8091
Cloudiver <1476763739@qq.com>
2019-05-31 20:12:13 +0800

    代码备份

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.utilpanel = new System.Windows.Forms.Panel();
+            this.TargetBox = new System.Windows.Forms.GroupBox();
+            this.target = new System.Windows.Forms.RichTextBox();
+            this.SourceBox = new System.Windows.Forms.GroupBox();
+            this.source = new System.Windows.Forms.RichTextBox();
+            this.baseToHex = new System.Windows.Forms.Button();
+            this.hexToBase = new System.Windows.Forms.Button();
+            this.hextodec = new System.Windows.Forms.Button();
+            this.dectohex = new System.Windows.Forms.Button();
+            this.delblank = new System.Windows.Forms.Button();
+            this.characternum = new System.Windows.Forms.Button();
+            this.crcCheck = new System.Windows.Forms.Button();
+            this.utilpanel.SuspendLayout();
+            this.TargetBox.SuspendLayout();
+            this.SourceBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // utilpanel
+            // 
+            this.utilpanel.Controls.Add(this.TargetBox);
+            this.utilpanel.Controls.Add(this.SourceBox);
+            this.utilpanel.Location = new System.Drawing.Point(12, 12);
+            this.utilpanel.Name = "utilpanel";
+            this.utilpanel.Size = new System.Drawing.Size(605, 397);
+            this.utilpanel.TabIndex = 0;
+            // 
+            // TargetBox
+            // 
+            this.TargetBox.Controls.Add(this.target);
+            this.TargetBox.Location = new System.Drawing.Point(12, 206);
+            this.TargetBox.Name = "TargetBox";
+            this.TargetBox.Size = new System.Drawing.Size(580, 165);
+            this.TargetBox.TabIndex = 1;
+            this.TargetBox.TabStop = false;
+            this.TargetBox.Text = "目标数据";
+            // 
+            // target
+            // 
+            this.target.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.target.Font = new System.Drawing.Font("宋体", 12F);
+            this.target.Location = new System.Drawing.Point(11, 20);
+            this.target.Name = "target";
+            this.target.ReadOnly = true;
+            this.target.Size = new System.Drawing.Size(558, 139);
+            this.target.TabIndex = 1;
+            this.target.Text = "";
+            // 
+            // SourceBox
+            // 
+            this.SourceBox.Controls.Add(this.source);
+            this.SourceBox.Location = new System.Drawing.Point(12, 12);
+            this.SourceBox.Name = "SourceBox";
+            this.SourceBox.Size = new System.Drawing.Size(580, 165);
+            this.SourceBox.TabIndex = 0;
+            this.SourceBox.TabStop = false;
+            this.SourceBox.Text = "源数据";
+            // 
+            // source
+            // 
+            this.source.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.source.Font = new System.Drawing.Font("宋体", 12F);
+            this.source.Location = new System.Drawing.Point(11, 20);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(558, 139);
+            this.source.TabIndex = 0;
+            this.source.Text = "";
+            //this.source.TextChanged += new MouseEventHandler(this.source_TextChanged);
+            // 
+            // baseToHex
+            // 
+            this.baseToHex.Font = new System.Drawing.Font("宋体", 10F);
+            this.baseToHex.Location = new System.Drawing.Point(640, 22);
+            this.baseToHex.Name = "baseToHex";
+            this.baseToHex.Size = new System.Drawing.Size(116, 46);
+            this.baseToHex.TabIndex = 1;
+            this.baseToHex.Text = "base64转16进制";
+            this.baseToHex.UseVisualStyleBackColor = true;
+            this.baseToHex.Click += new System.EventHandler(this.baseToHex_Click);
+            // 
+            // hexToBase
+            // 
+            this.hexToBase.Font = new System.Drawing.Font("宋体", 10F);
+            this.hexToBase.Location = new System.Drawing.Point(640, 74);
+            this.hexToBase.Name = "hexToBase";
+            this.hexToBase.Size = new System.Drawing.Size(116, 46);
+            this.hexToBase.TabIndex = 2;
+            this.hexToBase.Text = "16进制转base64";
+            this.hexToBase.UseVisualStyleBackColor = true;
+            this.hexToBase.Click += new System.EventHandler(this.hexToBase_Click);
+            // 
+            // hextodec
+            // 
+            this.hextodec.Font = new System.Drawing.Font("宋体", 10F);
+            this.hextodec.Location = new System.Drawing.Point(640, 126);
+            this.hextodec.Name = "hextodec";
+            this.hextodec.Size = new System.Drawing.Size(116, 46);
+            this.hextodec.TabIndex = 3;
+            this.hextodec.Text = "16进制转10进制";
+            this.hextodec.UseVisualStyleBackColor = true;
+            this.hextodec.Click += new System.EventHandler(this.hextodec_Click);
+            // 
+            // dectohex
+            // 
+            this.dectohex.Font = new System.Drawing.Font("宋体", 10F);
+            this.dectohex.Location = new System.Drawing.Point(640, 178);
+            this.dectohex.Name = "dectohex";
+            this.dectohex.Size = new System.Drawing.Size(116, 46);
+            this.dectohex.TabIndex = 4;
+            this.dectohex.Text = "10进制转16进制";
+            this.dectohex.UseVisualStyleBackColor = true;
+            this.dectohex.Click += new System.EventHandler(this.dectohex_Click);
+            // 
+            // delblank
+            // 
+            this.delblank.Font = new System.Drawing.Font("宋体", 10F);
+            this.delblank.Location = new System.Drawing.Point(640, 230);
+            this.delblank.Name = "delblank";
+            this.delblank.Size = new System.Drawing.Size(116, 46);
+            this.delblank.TabIndex = 5;
+            this.delblank.Text = "格式化字符串";
+            this.delblank.UseVisualStyleBackColor = true;
+            this.delblank.Click += new System.EventHandler(this.delblank_Click);
+            // 
+            // characternum
+            // 
+            this.characternum.Font = new System.Drawing.Font("宋体", 10F);
+            this.characternum.Location = new System.Drawing.Point(640, 282);
+            this.characternum.Name = "characternum";
+            this.characternum.Size = new System.Drawing.Size(116, 46);
+            this.characternum.TabIndex = 7;
+            this.characternum.Text = "字数统计";
+            this.characternum.UseVisualStyleBackColor = true;
+            this.characternum.Click += new System.EventHandler(this.characternum_Click);
+            // 
+            // crcCheck
+            // 
+            this.crcCheck.Font = new System.Drawing.Font("宋体", 11F);
+            this.crcCheck.Location = new System.Drawing.Point(640, 334);
+            this.crcCheck.Name = "crcCheck";
+            this.crcCheck.Size = new System.Drawing.Size(116, 46);
+            this.crcCheck.TabIndex = 8;
+            this.crcCheck.Text = "校验和";
+            this.crcCheck.UseVisualStyleBackColor = true;
+            this.crcCheck.Click += new System.EventHandler(this.crcCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 433);
+            this.Controls.Add(this.crcCheck);
+            this.Controls.Add(this.characternum);
+            this.Controls.Add(this.delblank);
+            this.Controls.Add(this.dectohex);
+            this.Controls.Add(this.hextodec);
+            this.Controls.Add(this.hexToBase);
+            this.Controls.Add(this.baseToHex);
+            this.Controls.Add(this.utilpanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "转换小工具";
+            this.utilpanel.ResumeLayout(false);
+            this.TargetBox.ResumeLayout(false);
+            this.SourceBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel utilpanel;
+        private System.Windows.Forms.GroupBox TargetBox;
+        private System.Windows.Forms.GroupBox SourceBox;
+        private System.Windows.Forms.Button baseToHex;
+        private System.Windows.Forms.Button hexToBase;
+        private System.Windows.Forms.Button hextodec;
+        private System.Windows.Forms.Button dectohex;
+        private System.Windows.Forms.Button delblank;
+        private RichTextBox target;
+        private RichTextBox source;
+        private Button characternum;
+        private Button crcCheck;
+
+
+
+
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        //base64转16进制字符串
+        private void baseToHex_Click(object sender, EventArgs e)
+        {
+            string DataSource = source.Text.Replace("\r", "").Replace("\n","").Replace(" ","");
+            try{
+                byte[] data = Convert.FromBase64String(DataSource);
+                string toHex = BitConverter.ToString(data).Replace("-", string.Empty);
+                //MessageBox.Show(hex);
+                target.Text = toHex;
+            }
+            catch (Exception ex) {
+               string str  = ex.ToString();
+               MessageBox.Show("请输入正确的base64字符!");
+            }
+            
+        }
+
+        //16进制字符串转base64
+        private void hexToBase_Click(object sender, EventArgs e)
+        {
+            string DataSource = source.Text.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            try
+            {
+                string toBase64 = Convert.ToBase64String(stringToByteArray(DataSource));
+                target.Text = toBase64;
+            }
+            catch (Exception ex)
+            {
+                string str = ex.ToString();
+                MessageBox.Show("数据不能是奇数位!");
+            }
+             
+        }
+
+        //字符串转数组
+         private byte[] stringToByteArray(string hexString)  
+        {
+
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+        }
+
+        //16进制转10进制
+         private void hextodec_Click(object sender, EventArgs e)
+         {
+            
+            string DataSource = source.Text.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            if (DataSource != "")
+            {
+                try
+                {
+                    long d = Convert.ToInt64(DataSource, 16);
+                    target.Text = d.ToString();
+                }
+
+                catch (Exception ex)
+                {
+                    string str = ex.ToString();
+                    MessageBox.Show("输入的数据有误!");
+                }
+            }
+            else
+            {
+                target.Text = "";
+            }
+             
+         }
+
+        //10进制转16进制
+         private void dectohex_Click(object sender, EventArgs e)
+         {
+            string DataSource = source.Text.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            if (DataSource != ""){
+                try{
+                long x = long.Parse(DataSource);
+                    string hex = Convert.ToString(x, 16);
+                    target.Text = hex;
+                }
+                catch (Exception ex)
+                {
+                    string str = ex.ToString();
+                    MessageBox.Show("输入的数据有误!");
+                }
+             }
+            else
+            {
+                target.Text = "";
+            }
+         }
+
+         private void delblank_Click(object sender, EventArgs e)
+         {
+             try {
+                 string DataSource = source.Text;
+                 string str = Regex.Replace(DataSource, "[^0-9A-Za-z]", "").Replace("0x","");
+                 target.Text = str;
+             }
+             catch (Exception ex)
+             {
+                 string str = ex.ToString();
+                 MessageBox.Show("输入的数据有误!");
+             }
+         }
+
+        //字符统计
+         private void characternum_Click(object sender, EventArgs e)
+         {
+             string DataSource = source.Text.Replace("\r", "").Replace("\n", "");
+             int charnum = DataSource.Length;
+             MessageBox.Show("字符个数:" + charnum);
+         }
+
+        //校验和
+         private void crcCheck_Click(object sender, EventArgs e)
+         {
+             string DataSource = source.Text.Replace("\r", "").Replace("\n", "");
+             if (DataSource != "")
+             {
+                 try
+                 {
+                     string[] res = Form1.Chop(DataSource, 2);
+                     int sum = 0;
+                     foreach (string ele in res)
+                     {
+                         int decValue = Convert.ToInt32(ele, 16);
+                         sum += decValue;
+                     }
+                     string strsum = Convert.ToString(sum, 16);
+                     string substr = strsum.Substring(strsum.Length - 2, 2);
+                     //int crc = Convert.ToInt32(substr, 16); #16进制转10进制long d = Convert.ToInt64(DataSource, 16);
+                     target.Text = substr;
+                 }
+                 catch (Exception ex)
+                 {
+                     string str = ex.ToString();
+                     MessageBox.Show("输入的数据有误!");
+                 }
+             }
+             else
+             {
+                 target.Text = "";
+             }
+         }
+
+        //按长度分割字符串
+         public static string[] Chop(string value, int length)
+         {
+             int strLength = value.Length;
+             int strCount = (strLength + length - 1) / length;
+             string[] result = new string[strCount];
+             for (int i = 0; i < strCount; ++i)
+             {
+                 result[i] = value.Substring(i * length, Math.Min(length, strLength));
+                 strLength -= length;
+             }
+             return result;
+         }
+
+        /*
+           右键复制粘贴
+         */
+         private void source_TextChanged(object sender, MouseEventArgs e)
+         {
+             if (e.Button == System.Windows.Forms.MouseButtons.Right)
+             {   //click event
+                 //MessageBox.Show("you got it!");
+                 ContextMenu contextMenu = new System.Windows.Forms.ContextMenu();
+                 MenuItem menuItem = new MenuItem("Cut");
+                 menuItem.Click += new EventHandler(CutAction);
+                 contextMenu.MenuItems.Add(menuItem);
+                 menuItem = new MenuItem("Copy");
+                 menuItem.Click += new EventHandler(CopyAction);
+                 contextMenu.MenuItems.Add(menuItem);
+                 menuItem = new MenuItem("Paste");
+                 menuItem.Click += new EventHandler(PasteAction);
+                 contextMenu.MenuItems.Add(menuItem);
+                 menuItem = new MenuItem("selectAll");
+                 menuItem.Click += new EventHandler(SelectAll);
+                 contextMenu.MenuItems.Add(menuItem);
+
+                 source.ContextMenu = contextMenu;
+             }
+         }
+
+        //剪切
+         void CutAction(object sender, EventArgs e)
+         {
+             source.Cut();
+         }
+
+        //复制
+         void CopyAction(object sender, EventArgs e)
+         {
+             Clipboard.SetText(source.SelectedText);
+         }
+
+        //粘贴
+         void PasteAction(object sender, EventArgs e)
+         {
+             if (Clipboard.ContainsText())
+             {
+                 source.Text
+                     += Clipboard.GetText(TextDataFormat.Text).ToString();
+             }
+         }
+
+        //全选
+         void SelectAll(object sender, EventArgs e)
+         {
+             source.SelectAll();
+             source.Focus();
+         }
+    }
+}
